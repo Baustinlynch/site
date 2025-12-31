abf1ce9e4d99aa56d33a5d50543499358cd59a17
baustinlynch <baustinlynch@protonmail.com>
2025-12-31 21:48:21 +0000

    changed line color in topo-config.js

--- topo-config.js
+++ topo-config.js
@@ -4,7 +4,7 @@ window.addEventListener('load', () => {
         seed: Math.floor(Math.random() * 2**31),
         step: 4,
         contourInterval: 0.03,
-        color: '#8a877aff',
+        color: 'rgba(138,135,122,0.49)',
         lineWidth: 1.5
     });
 });
